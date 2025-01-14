0f82508d0d76bf57100b5d205fa71b98b0c10401
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2025-01-14 20:32:01 +0800

    separate data file

---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const backgroundColorData = [
+  { id: 1, color: "bg-red-500" },
+  { id: 2, color: "bg-blue-500" },
+  { id: 3, color: "bg-green-500" },
+  { id: 4, color: "bg-orange-500" },
+];
+
+export const notes = [
+  {
+    title: "Grocery List",
+    description:
+      "Keep track of your grocery items and never forget a thing at the store.",
+    bgColor: "bg-red-500",
+  },
+
+  {
+    title: "Project Ideas",
+    description:
+      "Jot down all your project ideas, inspirations, and brainstorming sessions.",
+    bgColor: "bg-green-500",
+  },
+  {
+    title: "Meeting Notes",
+    description:
+      "Record important points, decisions, and action items from your meetings.",
+    bgColor: "bg-blue-500",
+  },
+  {
+    title: "Travel Plans",
+    description:
+      "Plan your next adventure with travel itineraries, packing lists, and must-visit spots.",
+    bgColor: "bg-green-500",
+  },
+  {
+    title: "Workout Routine",
+    description:
+      "Track your workout routines, sets, and progress for a healthier you.",
+    bgColor: "bg-orange-500",
+  },
+  {
+    title: "Book Reviews",
+    description:
+      "Write and store your thoughts on the books you've read and plan to read.",
+    bgColor: "bg-red-500",
+  },
+  {
+    title: "Recipe Collection",
+    description:
+      "Save your favorite recipes, including ingredients, instructions, and personal tweaks.",
+    bgColor: "bg-blue-500",
+  },
+  {
+    title: "Daily Journal",
+    description:
+      "Document your thoughts, experiences, and reflections each day.",
+    bgColor: "bg-orange-500",
+  },
+  {
+    title: "To-Do List",
+    description:
+      "Organize your tasks, set priorities, and check them off as you complete them.",
+    bgColor: "bg-green-500",
+  },
+  {
+    title: "Birthday Reminders",
+    description:
+      "Keep track of birthdays and special occasions to make sure you never miss an important date.",
+    bgColor: "bg-red-500",
+  },
+];
